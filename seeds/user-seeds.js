3ba6e7e7a7adb73cf828afde3a58c434abc7a851
Andrew <andy.p.123@hotmail.com>
2023-06-26 10:05:14 -0400

    template

---
+++ seeds/user-seeds.js
@@ -0,0 +1,32 @@
+const { User } = require("../../MVC-Tech-Blog/models");
+
+const userData = [
+  {
+    username: "andy_p",
+    password: "Password1",
+  },
+  {
+    username: "mahdi_s",
+    password: "Password2",
+  },
+  {
+    username: "rudri_j",
+    password: "Password3",
+  },
+  {
+    username: "tony_r",
+    password: "Password4",
+  },
+  {
+    username: "tony_l",
+    password: "Password5",
+  },
+  {
+    username: "john_s",
+    password: "Password6",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
