0043b83282e99647a51c279d8dd8e808b802ae8e
Andrew <andy.p.123@hotmail.com>
2023-06-27 09:52:26 -0400

    change

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,18 +2,17 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    comment_text: "Lorem ipsum dolor sit amet",
+    comment_text: "Amazing",
     user_id: 1,
     post_id: 1,
   },
   {
-    comment_text: "consectetur adipiscing elit",
+    comment_text: "Interesting",
     user_id: 2,
     post_id: 2,
   },
   {
-    comment_text:
-      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    comment_text: "It's about time!!",
     user_id: 3,
     post_id: 3,
   },
